8c41ef2db3fda15bdcd0619d1443bd66d9a09f2b
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2023-08-20 20:17:13 +0530

    Create vowels.py

---
+++ vowels.py
@@ -0,0 +1,6 @@
+name="ayonija thinks exceptionally"
+for i in range(len(name)):
+    if (name[i]=='a' or name[i]=='e' or name[i]=='i' or name[i]=='u' or name[i]=='o'):
+        continue
+    
+    print(name[i])
